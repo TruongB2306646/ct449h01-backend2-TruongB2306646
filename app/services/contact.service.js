4d5fc242d4e05890f2482de76bd7169e2c9ce44c
TruongB2306646 <truongb2306646@student.ctu.edu.vn>
2026-06-30 12:29:31 +0700

    Cau hinh ket noi MongoDB

---
+++ app/services/contact.service.js
@@ -0,0 +1,9 @@
+class ContactService {
+  constructor(client) {
+    this.Contact = client.db().collection("contacts");
+  }
+
+  // Định nghĩa các phương thức truy xuất CSDL sử dụng mongodb API
+}
+
+module.exports = ContactService;
